c3db7b4fcc0a618460be256033b1760e97b84187
David Mason <david_mason_@hotmail.co.uk>
2023-12-06 22:20:01 +0000

    Day 5 complete!

--- Day_5/main.js
+++ Day_5/main.js
@@ -31,101 +31,78 @@ const log = console.log.bind(console);
 // // // Part 2
 // Functions
 
-function numberFinder(rangeArray, compArray) {
+function comparison(range, compArray) {
   const newRangeArray = [];
-  rangeArray.map((range) => {
-    const newRange = []
-    log("Finder function range: ", range);
-    //   log("Finder function comp array: ", compArray)
-    const lowerValue = range[0];
-    const upperValue = range[0] + range[1] - 1;
-      log("Finder function lower value: ", lowerValue)
-      log("Finder function upper value: ", upperValue)
+  const newRange = [];
 
-      // const test = []
-      // for(let i = 0; i < range[1]; i++) {
-      //   test.push(lowerValue + i)
-      // }
-      // log("test", test)
+  const lowerValue = range[0];
+  const upperValue = range[0] + range[1] - 1;
 
-    compArray.map(compRange => {
-      const numberArray = compRange.split(" ").map((string) => Number(string));
-      const lowerCompValue = numberArray[1];
-      const upperCompValue = numberArray[1] + numberArray[2] - 1;
-      
-      if(lowerValue > lowerCompValue && upperValue < upperCompValue) {
-        newRange.push([(lowerValue - lowerCompValue + numberArray[0]), range[1]])
-      } else if(lowerValue > lowerCompValue && lowerValue < upperCompValue && upperValue > upperCompValue) {
-        const newLowerValue = lowerValue - lowerCompValue + numberArray[0];
-        const newLowerRange = upperCompValue - lowerValue;
-        const newUpperValue = upperCompValue;
-        const newUpperRange = upperValue - upperCompValue;
-        if(newRange.length > 0) {
-          newRange.map((newRanges, newRangesIndex) => {
-            newUpperValue - newUpperRange === newRanges[0] ? (newRange[newRangesIndex] = [newLowerValue, newLowerRange]) :
-            
-          })
-          // need to do the comparison of the new upper range incase itr has already been changed
-        }
+  compArray.map((compRange) => {
+    const numberArray = compRange.split(" ").map((string) => Number(string));
+    const lowerCompValue = numberArray[1];
+    const upperCompValue = numberArray[1] + numberArray[2] - 1;
 
-      }
-      /* Going through the scenarios of what could occur with the range and creating new ranges to be pushed up to the newRangeArray*/
+    if (lowerValue >= lowerCompValue && upperValue <= upperCompValue) {
+      newRangeArray.push([
+        lowerValue - lowerCompValue + numberArray[0],
+        range[1],
+      ]);
+    } else if (
+      lowerValue >= lowerCompValue &&
+      lowerValue <= upperCompValue &&
+      upperValue > upperCompValue
+    ) {
+      const newLowerValue = lowerValue - lowerCompValue + numberArray[0];
+      const newLowerRange = upperCompValue - lowerValue + 1;
+      const newUpperValue = upperCompValue + 1;
+      const newUpperRange = upperValue - upperCompValue;
+
+      newRangeArray.push([newLowerValue, newLowerRange]);
+      newRange.push([newUpperValue, newUpperRange]);
+    } else if (
+      lowerValue < lowerCompValue &&
+      upperValue >= lowerCompValue &&
+      upperValue <= upperCompValue
+    ) {
+      const newUpperValue = numberArray[0];
+      const newUpperRange = upperValue - lowerCompValue + 1;
+      const newLowerValue = lowerValue;
+      const newLowerRange = lowerCompValue - lowerValue;
+
+      newRangeArray.push([newUpperValue, newUpperRange]);
+      newRange.push([newLowerValue, newLowerRange]);
+    } else if (lowerValue < lowerCompValue && upperValue > upperCompValue) {
+      const newInnerValue = numberArray[0];
+      const newInnerRange = numberArray[2];
+      const newLowerValue = lowerValue;
+      const newLowerRange = lowerCompValue - lowerValue;
+      const newUpperValue = upperCompValue + 1;
+      const newUpperRange = upperValue - upperCompValue;
+
+      newRangeArray.push([newInnerValue, newInnerRange]);
+      newRange.push([newLowerValue, newLowerRange]);
+      newRange.push([newUpperValue, newUpperRange]);
+    }
+  });
+  newRangeArray.length === 0 && newRange.push(range);
+  return [newRangeArray, newRange];
+}
+
+function numberFinder(seedIndexArray, compArray) {
+  const newRangeArray = [];
+
+  seedIndexArray.map((singleRange) => {
+    const [forRangeArray, forRange] = comparison(singleRange, compArray);
+    newRangeArray.push(...forRangeArray);
+    // log("forRange: ", forRange);
+    forRange.map((rangeBit) => {
+      const [unNeeded, unAltered] = comparison(rangeBit, compArray);
+      newRangeArray.push(...unAltered);
     });
+  });
 
-    log("New range: ", newRange);
-
-
-    });
-    
-  //     const minMaxArray = [];
-  //   const compNumberArray = [];
-  //   compArray.map((string) => {
-  //     const numberArray = string.split(" ").map((string) => Number(string));
-  //     compNumberArray.push(numberArray);
-  //     minMaxArray.push(numberArray[1]);
-  //     minMaxArray.push(numberArray[1] + numberArray[2] - 1);
-  //   });
-  //   log("Range array: ", rangeArray);
-
-  //   const compMinValue = minMaxArray.sort((a, b) => a - b)[0];
-  //   const compMaxValue = minMaxArray.sort((a, b) => a - b)[
-  //     minMaxArray.length - 1
-  //   ];
-  //     // log("Min and Max array: ", minMaxArray, minMaxArray.sort((a, b) => a - b)[0], minMaxArray.sort((a, b) => a - b)[minMaxArray.length -1])
-  //     // log("Min", compMinValue)
-  //     // log("Max", compMaxValue)
-
-  //   let withinRange = false;
-  //   let partialWithinRange = false;
-  //   let surroundingRange = false;
-
-  //   lowerValue >= compMinValue &&
-  //     lowerValue <= compMaxValue &&
-  //     (withinRange = true);
-  //   upperValue >= compMinValue &&
-  //     upperValue <= compMaxValue &&
-  //     (withinRange = true);
-  //   lowerValue >= compMinValue &&
-  //     lowerValue <= compMaxValue &&
-  //     upperValue > compMaxValue &&
-  //     (partialWithinRange = true);
-  //   upperValue >= compMinValue &&
-  //     upperValue <= compMaxValue &&
-  //     lowerValue < compMinValue &&
-  //     (partialWithinRange = true);
-  //   lowerValue < compMinValue &&
-  //     upperValue > compMaxValue &&
-  //     (surroundingRange = true);
-  // });
-
-  //   !surroundingRange && !partialWithinRange && withinRange ? (range[0] = (range[0] - min))
-
-  //         const diff = initialNumber >= rangeArray[1] && initialNumber < rangeArray[1] + rangeArray[2] && initialNumber - rangeArray[1];
-  //         typeof(diff) === "number" && (newValue = rangeArray[0] + diff);
-
-  //   return newValue ? newValue : range;
-  /* TODO: Work out how to determine the change in the value. May have to be in the compArray map again to find the comp start value and difference to be added to the change */
-  /* TODO: OR map through individual numbers again but just change the local array to not an array and compare the values so that is always just one. */
+  return newRangeArray;
 }
 
 // // Main function
@@ -143,59 +120,24 @@ function trueLocationFinder(
   const localArray = [];
 
   const seedArray = seedsString.split(" ");
-  //   log(seedArray)
   const seedNumberArray = seedArray.map((seed) => Number(seed));
-  //   log("Seed Numbers", seedNumberArray);
   const seedIndexArray = [];
 
   for (let i = 0; i < seedArray.length; i += 2) {
     seedIndexArray.push([seedNumberArray[i], seedNumberArray[i + 1]]);
-  }
-  //   log("Seed index array: ", seedIndexArray);
-
-  // log("seeds:",seedArray)
-  // log("_________________________________________________________________")
-
-  seedIndexArray.map((seedRange) => {
-    log("Seed range: ", seedRange);
-
-    const seedRangeLocal = [];
-
-    // log("Seed number: ", seedNumber)
-    // log("Soil")
-    const soilNumber = numberFinder([seedRange], seedSoilArray);
-    // log("Seed Number: ", seedNumber, " | Soil number: ", soilNumber)
-    // log("Fert")
-    const fertilizerNumber = numberFinder(soilNumber, soilFertilizerArray);
-    // log("Soil Number: ", soilNumber, " | Fert number: ", fertilizerNumber)
-    log("Water");
-    const waterNumber = numberFinder(fertilizerNumber, fertilizerWaterArray);
-    // log("Fert Number: ", fertilizerNumber, " | Water number: ", waterNumber)
-    // log("Light")
-    const lightNumber = numberFinder(waterNumber, waterLighArray);
-    // log("Water Number: ", waterNumber, " | Light number: ", lightNumber)
-    // log("Temp")
-    const tempNumber = numberFinder(lightNumber, lightTempArray);
-    // log("Light Number: ", lightNumber, " | Temp number: ", tempNumber)
-    // log("Humid")
-    const humidNumber = numberFinder(tempNumber, tempHumidArray);
-    // log("Temp Number: ", tempNumber, " | Humid number: ", humidNumber)
-    // log("Local")
-    const localNumber = numberFinder(humidNumber, humidLocalArray);
-    // log("Humid Number: ", humidNumber, " | Local number: ", localNumber)
-    seedRangeLocal.push(localNumber);
-
-    // log("_________________________________________________________________")
-
-    // log("Seed range local", seedRangeLocal.sort((a, b) => a - b))
-    localArray.push(seedRangeLocal.sort((a, b) => a - b)[0]);
+  };
+  const soilNumber = numberFinder(seedIndexArray, seedSoilArray);
+  const fertilizerNumber = numberFinder(soilNumber, soilFertilizerArray);
+  const waterNumber = numberFinder(fertilizerNumber, fertilizerWaterArray);
+  const lightNumber = numberFinder(waterNumber, waterLighArray);
+  const tempNumber = numberFinder(lightNumber, lightTempArray);
+  const humidNumber = numberFinder(tempNumber, tempHumidArray);
+  const localNumber = numberFinder(humidNumber, humidLocalArray);
+  localNumber.map((localRange) => {
+    localArray.push(localRange[0]);
   });
-  //   log("Seeds:", seedArray);
-  // log("Location array: ", localArray);
-  //   log(
-  //     "Sorted locations: ",
-  //     localArray.sort((a, b) => a - b)
-  //   );
+
+
   log("First location: ", localArray.sort((a, b) => a - b)[0]);
 }
 
@@ -209,7 +151,7 @@ trueLocationFinder(
   smallTemperatureToHumidityMap,
   smallHumidityToLocationMap
 );
-// trueLocationFinder(seeds, seedToSoilMap, soilToFertilizerMap, fertilizerToWaterMap, waterToLightMap, lightToTemperatureMap, temperatureToHumidityMap, humidityToLocationMap)
+trueLocationFinder(seeds, seedToSoilMap, soilToFertilizerMap, fertilizerToWaterMap, waterToLightMap, lightToTemperatureMap, temperatureToHumidityMap, humidityToLocationMap)
 
 // // // Part 1
 
@@ -270,206 +212,4 @@ trueLocationFinder(
 //     log("Sorted locations: ", localArray.sort((a, b) => a - b))
 //     log("First location: ", localArray.sort((a, b) => a - b)[0])
 
-// }
-
-// // // Part 1.5 --- Don't use full data on
-
-// function numberFinder(initialNumber, compArray) {
-//     let newValue;
-
-//     compArray.map((string) => {
-//       const rangeArray = string.split(" ").map((string) => Number(string));
-//       // log("Single range:", rangeArray)
-
-//       const diff =
-//         initialNumber >= rangeArray[1] &&
-//         initialNumber < rangeArray[1] + rangeArray[2] &&
-//         initialNumber - rangeArray[1];
-//       // diff && log("In range", initialNumber, "of", rangeArray[1], "and",  rangeArray[1]+rangeArray[2])
-
-//       typeof diff === "number" && (newValue = rangeArray[0] + diff);
-//       // log(typeof(diff))
-//     });
-//     return newValue ? newValue : initialNumber;
-//     // log("Soil number", soilNumber)
-//   }
-
-//   function trueLocationFinder(
-//     seedsString,
-//     seedSoilArray,
-//     soilFertilizerArray,
-//     fertilizerWaterArray,
-//     waterLighArray,
-//     lightTempArray,
-//     tempHumidArray,
-//     humidLocalArray
-//   ) {
-//     const localArray = [];
-
-//     const seedArray = seedsString.split(" ");
-//     const seedNumberArray = seedArray.map((seed) => Number(seed));
-//     //   log("Seed Numbers", seedNumberArray);
-//     const seedIndexArray = [];
-
-//     for (let i = 0; i < seedArray.length; i += 2) {
-//       seedIndexArray.push([seedNumberArray[i], seedNumberArray[i + 1]]);
-//     }
-//   //   log("Seed index array: ", seedIndexArray);
-
-//     // log("seeds:",seedArray)
-//     // log("_________________________________________________________________")
-
-//     seedIndexArray.map((seedRange) => {
-
-//       // // Testing
-//       // const rangeByHundred = Math.floor(seedRange[1] / 100)
-//       // console.log(rangeByHundred)
-
-//       // log("Seed range: ", seedRange);
-//       const seedArray = [];
-//       for (let i = 0; i < seedRange[1]; i++) {
-//         try {
-//           seedArray.push(seedRange[0] + i);
-//         } catch (error) {
-//           log("Failed", seedRange[0], seedRange[1], rangeByHundred);
-//           return;
-//         }
-//       }
-//       // log("Seed array: ", seedArray);
-
-//       const seedRangeLocal = [];
-
-//       seedArray.map((seedNumber) => {
-//         // log("Seed number: ", seedNumber)
-//         // log("Soil")
-//         const soilNumber = numberFinder(seedNumber, seedSoilArray);
-//         // log("Seed Number: ", seedNumber, " | Soil number: ", soilNumber)
-//         // log("Fert")
-//         const fertilizerNumber = numberFinder(soilNumber, soilFertilizerArray);
-//         // log("Soil Number: ", soilNumber, " | Fert number: ", fertilizerNumber)
-//         // log("Water")
-//         const waterNumber = numberFinder(fertilizerNumber, fertilizerWaterArray);
-//         // log("Fert Number: ", fertilizerNumber, " | Water number: ", waterNumber)
-//         // log("Light")
-//         const lightNumber = numberFinder(waterNumber, waterLighArray);
-//         // log("Water Number: ", waterNumber, " | Light number: ", lightNumber)
-//         // log("Temp")
-//         const tempNumber = numberFinder(lightNumber, lightTempArray);
-//         // log("Light Number: ", lightNumber, " | Temp number: ", tempNumber)
-//         // log("Humid")
-//         const humidNumber = numberFinder(tempNumber, tempHumidArray);
-//         // log("Temp Number: ", tempNumber, " | Humid number: ", humidNumber)
-//         // log("Local")
-//         const localNumber = numberFinder(humidNumber, humidLocalArray);
-//         // log("Humid Number: ", humidNumber, " | Local number: ", localNumber)
-//         seedRangeLocal.push(localNumber);
-
-//         // log("_________________________________________________________________")
-//       });
-//       // log("Seed range local", seedRangeLocal.sort((a, b) => a - b))
-//       localArray.push(seedRangeLocal.sort((a, b) => a - b)[0])
-
-//     });
-//     //   log("Seeds:", seedArray);
-//       log("Location array: ", localArray);
-//     //   log(
-//     //     "Sorted locations: ",
-//     //     localArray.sort((a, b) => a - b)
-//     //   );
-//     log("First location: ", localArray.sort((a, b) => a - b)[0]);
-//   }
-
-// // // Part 1.5 --- Don't use full data on
-
-// function numberFinder(initialNumber, compArray) {
-//   let newValue;
-
-//   compArray.map((string) => {
-//     const rangeArray = string.split(" ").map((string) => Number(string));
-//     // log("Single range:", rangeArray)
-
-//     const diff =
-//       initialNumber >= rangeArray[1] &&
-//       initialNumber < rangeArray[1] + rangeArray[2] &&
-//       initialNumber - rangeArray[1];
-//     // diff && log("In range", initialNumber, "of", rangeArray[1], "and",  rangeArray[1]+rangeArray[2])
-
-//     typeof diff === "number" && (newValue = rangeArray[0] + diff);
-//     // log(typeof(diff))
-//   });
-//   return newValue ? newValue : initialNumber;
-//   // log("Soil number", soilNumber)
-// }
-
-// function trueLocationFinder(
-//   seedsString,
-//   seedSoilArray,
-//   soilFertilizerArray,
-//   fertilizerWaterArray,
-//   waterLighArray,
-//   lightTempArray,
-//   tempHumidArray,
-//   humidLocalArray
-// ) {
-//   let localArray;
-
-//   const seedArray = seedsString.split(" ");
-//   const seedNumberArray = seedArray.map((seed) => Number(seed));
-//   //   log("Seed Numbers", seedNumberArray);
-//   const seedIndexArray = [];
-
-//   for (let i = 0; i < seedArray.length; i += 2) {
-//     seedIndexArray.push([seedNumberArray[i], seedNumberArray[i + 1]]);
-//   }
-// //   log("Seed index array: ", seedIndexArray);
-
-//   // log("seeds:",seedArray)
-//   // log("_________________________________________________________________")
-
-//   seedIndexArray.map((seedRange) => {
-
-//     // // Testing
-//     // const rangeByHundred = Math.floor(seedRange[1] / 100)
-//     // console.log(rangeByHundred)
-
-//     // log("Seed range: ", seedRange);
-//     for (let i = 0; i < seedRange[1]; i++) {
-//         const seedNumber = seedRange[0] + i;
-
-//       // log("Seed number: ", seedNumber)
-//       // log("Soil")
-//       const soilNumber = numberFinder(seedNumber, seedSoilArray);
-//       // log("Seed Number: ", seedNumber, " | Soil number: ", soilNumber)
-//       // log("Fert")
-//       const fertilizerNumber = numberFinder(soilNumber, soilFertilizerArray);
-//       // log("Soil Number: ", soilNumber, " | Fert number: ", fertilizerNumber)
-//       // log("Water")
-//       const waterNumber = numberFinder(fertilizerNumber, fertilizerWaterArray);
-//       // log("Fert Number: ", fertilizerNumber, " | Water number: ", waterNumber)
-//       // log("Light")
-//       const lightNumber = numberFinder(waterNumber, waterLighArray);
-//       // log("Water Number: ", waterNumber, " | Light number: ", lightNumber)
-//       // log("Temp")
-//       const tempNumber = numberFinder(lightNumber, lightTempArray);
-//       // log("Light Number: ", lightNumber, " | Temp number: ", tempNumber)
-//       // log("Humid")
-//       const humidNumber = numberFinder(tempNumber, tempHumidArray);
-//       // log("Temp Number: ", tempNumber, " | Humid number: ", humidNumber)
-//       // log("Local")
-//       const localNumber = numberFinder(humidNumber, humidLocalArray);
-//       // log("Humid Number: ", humidNumber, " | Local number: ", localNumber)
-//       localArray === undefined && (localArray = localNumber);
-//       localArray > localNumber && (localArray = localNumber);
-//       // log(localArray)
-
-//       // log("_________________________________________________________________")
-//     };
-//   });
-//   //   log("Seeds:", seedArray);
-//     log("Location array: ", localArray);
-//   //   log(
-//   //     "Sorted locations: ",
-//   //     localArray.sort((a, b) => a - b)
-//   //   );
-//   // log("First location: ", localArray.sort((a, b) => a - b)[0]);
 // }
