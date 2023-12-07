edc41080355f87bc767823d648c95c714946a1a1
David Mason <david_mason_@hotmail.co.uk>
2023-12-07 09:21:43 +0000

    Day 6 Part 2 continued

--- Day_6/main.js
+++ Day_6/main.js
@@ -2,8 +2,58 @@ import { timesArray, distancesArray } from "./data.js";
 
 const log = console.log.bind(console)
 
-const singleTime = [7, 15, 30];
-const singleDistance = [9, 40, 200];
+const timeExample = [7, 15, 30];
+const distanceExample = [9, 40, 200];
+
+
+// // // Part 2
+
+function theWayToWin (times, distances) {
+    const trueTime = Number(times.join(""));
+    const trueDistance = Number(distances.join(""));
+    log("True time: ", trueTime, "|  True distance: ", trueDistance)
+
+    const halfTime = Math.floor(trueTime/2)
+
+    const sortArray = [halfTime, Math.floor(halfTime/2)];
+    log(sortArray)
+
+    while(sortArray[0]-sortArray[1] !== 1 || sortArray[0]-sortArray[1] !== -1) {
+        const timeRemaining = trueTime - sortArray[1];
+        const distanceTravelled = timeRemaining * sortArray[1];
+        if(distanceTravelled <= trueDistance) {
+            sortArray[1] = Math.floor(((sortArray[0] - sortArray[1])/2) + sortArray[1]);
+        } else {
+            const newValue = Math.floor(sortArray[1]-((sortArray[0] - sortArray[1])/2))
+            sortArray[0] = sortArray[1];
+            sortArray[1] = newValue;
+        }
+    };
+    
+    const lowestWin = (trueTime - sortArray[1])*sortArray[1] > distanceTravelled ? sortArray[1] : sortArray[0];
+    const highestWin = (timeRemaining - (lowestWin + halfTime + 1))*(lowestWin + halfTime) > distanceTravelled ? (lowestWin + 1) :
+                                (timeRemaining - (lowestWin + halfTime))*(lowestWin + halfTime) > distanceTravelled ? lowestWin : lowestWin - 1;
+    const numberOfWins = highestWin - lowestWin;
+    log(numberOfWins)
+}
+
+
+
+// theWayToWin(timeExample, distanceExample);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // // // Part 1
 
@@ -25,5 +75,5 @@ function waysToWin(times, distances) {
 
 };
 
-waysToWin(singleTime, singleDistance);
-waysToWin(timesArray, distancesArray);
+// waysToWin(timeExample, distanceExample);
+// waysToWin(timesArray, distancesArray);
